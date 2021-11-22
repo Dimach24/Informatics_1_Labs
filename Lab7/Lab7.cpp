f3792cacab5cd03b1258e5f6464fd591625aa451
D.mon 24 <dchaminov@ya.ru>
2021-11-22 16:19:22 +0300

    algorythm got done

--- Lab7/Lab7.cpp
+++ Lab7/Lab7.cpp
@@ -9,12 +9,39 @@ int main()
 	Phonebook pb;
 	std::string answer;
 	if (did_user_accept("Do you want to use file as an input?")) {
-		std::cout << "Input file path:";
 		std::ifstream fin;
+		//file opening
+		{
+			while (!fin.is_open())
+				std::cout << "Input file path:";
+			getline(std::cin, answer, '\n');
+			fin.open(answer);
+			if (!fin.is_open()) {
+				std::cerr << "Can't open the file" << std::endl;
+			}
+		}
+		pb.inputFromFile(fin);
+		fin.close();
+	} else {
+		pb.input("0");
 	}
-	
-	std::ofstream fout;
-	pb.print(50, std::cout, '.');	// print phonebook
+	if (did_user_accept("Do you want to use file as an output?")) {
+		std::ofstream fout;
+		//file opening
+		{
+			while (!fout.is_open())
+				std::cout << "Input file path:";
+			getline(std::cin, answer, '\n');
+			fout.open(answer);
+			if (!fout.is_open()) {
+				std::cerr << "Can't open the file" << std::endl;
+			}
+		}
+		pb.print(40, fout, '.');
+		fout.close();
+	} else {
+		pb.print(40, std::cout, '.');
+	}
 	if (system(NULL)) {				// if 'system' function is available
 		system("pause");			// do not close console
 	}
