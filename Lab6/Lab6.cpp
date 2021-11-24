bd65ffb243842f91c201d288ea4b9f83f49966a6
D.mon 24 <dchaminov@ya.ru>
2021-11-24 14:33:10 +0300

    Lab6 comments

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
-	Phonebook pb;
-	pb.input();
+	Phonebook pb;					// phonebook declaration (default constructor called)
+	pb.input();						// input the phonebook
 	pb.print(50, std::cout, '.');	// print phonebook
 	if (system(NULL)) {				// if 'system' function is available
 		system("pause");			// do not close console
