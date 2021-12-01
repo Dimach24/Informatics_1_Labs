cac6b9b5b59c497eed51160096864c398fe25877
D.mon 24 <dchaminov@ya.ru>
2021-12-01 16:39:58 +0300

    new project for import-export google csv for testing
    changes required adding new project

---
+++ import-export/import-export.cpp
@@ -0,0 +1,22 @@
+﻿// import-export.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "../libs/PhoneNumerCPP.h"
+#include <fstream>
+#include <process.h>
+#include <locale>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Phonebook pb;
+    std::ifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
+    pb.importPhonebook(fin);
+    pb.print(0,std::cout,'.');
+    if (system(NULL)) {				// if 'system' function is available
+        system("pause");			// do not close console
+    }
+    fin.close();
+    return 0;
+}
