6411b2b15430907aaa82d7295a3f50d355491f64
D.mon 24 <dchaminov@ya.ru>
2021-12-04 00:11:44 +0300

    wstreams checking
    do not work...
    again...

--- import-export/import-export.cpp
+++ import-export/import-export.cpp
@@ -5,17 +5,33 @@
 #include "../libs/PhoneNumerCPP.h"
 #include <fstream>
 #include <process.h>
+#include <fcntl.h>
+#include <io.h>
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    //setlocale(LC_ALL, "utf-8");
     Phonebook pb;
-    std::ifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
-    pb.importPhonebook(fin);
+    std::wifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
+    /*pb.importPhonebook(fin);
     pb.print(0,std::cout,'.');
     if (system(NULL)) {				// if 'system' function is available
         system("pause");			// do not close console
-    }
+    }*/
+    wchar_t buffer[1024];
+    std::wstring s;
+    fin.getline(buffer,1024);
+    fin.getline(buffer,1024);
+    s = buffer;
+    std::wcout << s <<'\n';
     fin.close();
+    if (system(NULL)) {				// if 'system' function is available
+        system("pause");			// do not close console
+    }
+    std::wofstream fout(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts_o.csv)");
+    fout.write(s.c_str(),s.size());
+    fout.close();
     return 0;
 }
