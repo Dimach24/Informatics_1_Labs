c4fa02847873189ccbaa118baeca8bcb53ec8aa6
D.mon 24 <dchaminov@ya.ru>
2021-12-04 01:05:57 +0300

    wide to multibyte

--- import-export/import-export.cpp
+++ import-export/import-export.cpp
@@ -5,33 +5,37 @@
 #include "../libs/PhoneNumerCPP.h"
 #include <fstream>
 #include <process.h>
+#include <atlconv.h>
 #include <fcntl.h>
 #include <io.h>
 
 int main()
 {
-    _setmode(_fileno(stdout), _O_U16TEXT);
-    _setmode(_fileno(stdin), _O_U16TEXT);
-    //setlocale(LC_ALL, "utf-8");
-    Phonebook pb;
-    std::wifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
-    /*pb.importPhonebook(fin);
-    pb.print(0,std::cout,'.');
-    if (system(NULL)) {				// if 'system' function is available
-        system("pause");			// do not close console
-    }*/
-    wchar_t buffer[1024];
-    std::wstring s;
-    fin.getline(buffer,1024);
-    fin.getline(buffer,1024);
-    s = buffer;
-    std::wcout << s <<'\n';
-    fin.close();
-    if (system(NULL)) {				// if 'system' function is available
-        system("pause");			// do not close console
-    }
-    std::wofstream fout(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts_o.csv)");
-    fout.write(s.c_str(),s.size());
-    fout.close();
-    return 0;
+	setlocale(LC_ALL, "rus");
+	//Phonebook pb;
+	std::wifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
+	/*pb.importPhonebook(fin);
+	pb.print(0,std::cout,'.');
+	if (system(NULL)) {				// if 'system' function is available
+		system("pause");			// do not close console
+	}*/
+	std::wstring s;
+	getline(fin, s);
+	getline(fin, s);
+	char buffer[1024];
+	const char d = '@';
+	LPBOOL t = new BOOL(1);
+	WideCharToMultiByte(1256, WC_COMPOSITECHECK, s.c_str(), -1, buffer, 1024, &d, t);
+	delete t;
+	std::cout << buffer << '\n';
+	fin.close();
+	if (system(NULL)) {				// if 'system' function is available
+		system("pause");			// do not close console
+	}
+	std::wofstream fout(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts_o.csv)");
+
+
+	fout.write(s.c_str(), s.size());
+	fout.close();
+	return 0;
 }
