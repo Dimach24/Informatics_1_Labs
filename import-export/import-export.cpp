74b4fa7e491eeafd0a15f02d95fcda585c0a64ca
D.mon 24 <dchaminov@ya.ru>
2021-12-04 13:59:49 +0300

    first action

--- import-export/import-export.cpp
+++ import-export/import-export.cpp
@@ -4,20 +4,72 @@
 #include <iostream>
 #include <fstream>
 #include <process.h>
-
+#include "../libs/PhoneNumerCPP.h"
 
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	std::ifstream fin(R"(C:\Users\D.mon\Documents\учеба\инфа\Chaminov_Dmeatry\Labs\Release\files\contacts.csv)");
-	std::string s;
 	Phonebook pb;
-	pb.importPhonebook(fin);
-	fin.close();
-	pb.print_utf();
-	if (system(NULL)) {				// if 'system' function is available
-		system("pause");			// do not close console
+	setlocale(LC_ALL, "rus");
+start_case:
+	std::cout << "Выбирете действие:" << std::endl;
+	std::ifstream fin;
+	std::string answer;
+	switch (choose_list(3, "Ввести с клавиатуры\0Ввести из файла\0Импортировать\0")) {
+	case 1:
+		pb.input("0",std::cin,std::cout,"Введите имя\n>>> ","Введите номер\n>>> ");
+		break;
+	case 2:
+		while (!fin.is_open()) {
+			std::cout << "Введите путь к файлу:" << std::endl
+				<< ">>> ";
+			getline(std::cin, answer);
+			fin.open(answer);
+			if (!fin.is_open()) {
+				std::cout << "Не удалось открыть файл :(" << std::endl
+					<< "Выбирите действие:" << std::endl;
+				switch (choose_list(3, "Попробовать снова\0Вернуться к началу\0Завершить выполнение")) {
+				case 1:
+					break;
+				case 2:
+					goto start_case;
+					break;
+				case 3:
+					return 0;
+					break;
+				}
+			}
+		}
+		pb.inputFromFile(fin);
+		break;
+	case 3:
+		while (!fin.is_open()) {
+			std::cout << "Введите путь к файлу:" << std::endl
+				<< ">>> ";
+			getline(std::cin, answer);
+			fin.open(answer);
+			if (!fin.is_open()) {
+				std::cout << "Не удалось открыть файл :(" << std::endl
+					<< "Выбирите действие:" << std::endl;
+				switch (choose_list(3, "Попробовать снова\0Вернуться к началу\0Завершить выполнение")) {
+				case 1:
+					break;
+				case 2:
+					goto start_case;
+					break;
+				case 3:
+					return 0;
+					break;
+				}
+			}
+		}
+		pb.importPhonebook(fin);
+		break;
 	}
 	return 0;
 }
+//TODO
+// добавить флаг записи (про utf-8) и 
+// его обработчик внутри функции вывода
+// для совмещения источников записей
+
