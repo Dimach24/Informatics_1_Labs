9ede9fe7ab7e5dd84be6257b42feaf825103523e
D.mon 24 <dchaminov@ya.ru>
2021-12-10 19:42:32 +0300

    utf end of the string fixed and comments

--- libs/utf-8Converting.cpp
+++ libs/utf-8Converting.cpp
@@ -4,10 +4,11 @@
 void UTFconvert(const char str[], size_t n, char* out, char fill_unknown_with)
 {
 	if (!n) { n = strlen(str); }			// calculate string length if n is a NULL						
-	wchar_t* wide_buffer = nullptr;			// buffer declaration and allocation
+	wchar_t* wide_buffer = nullptr;			// buffer declaration
 	size_t wn =MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, str, n, wide_buffer, 0);
 											// calculate wide char c-string size
-	wide_buffer = new wchar_t[wn];			// buffer allocate
+	wide_buffer = new wchar_t[wn+1];		// buffer allocation
+	wide_buffer[wn] = L'\0';				// end of the string
 	MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, str, n, wide_buffer, wn);
 						// convert string from utf-8 to utf-16 and write it to the buffer
 	WideCharToMultiByte(1251, NULL, wide_buffer, -1, out, n, &fill_unknown_with, NULL);
@@ -44,13 +45,15 @@ std::string StringToUTF(std::string s)
 	wchar_t* wide_buffer=nullptr;		// wchar pointer declaration
 	size_t n = MultiByteToWideChar(1251, NULL, s.c_str(), -1, wide_buffer, 0);
 										// calculate size of the c-wstring
-	wide_buffer = new wchar_t[n];		// buffer memory allocation
+	wide_buffer = new wchar_t[n+1];		// buffer memory allocation
+	wide_buffer[n]=L'\0';				// end of the string
 	MultiByteToWideChar(1251, NULL, s.c_str(), -1, wide_buffer, n);
 										// converting from codepage 1251 to utf 16 and writing it 
 										// to the buffer with saving amount of wchars
 	char* buffer = nullptr;				// char pointer declaration
 	n=WideCharToMultiByte(CP_UTF8, NULL, wide_buffer, n, buffer, 0, NULL, NULL);
-	buffer = new char[n];				// buffer allocation
+	buffer = new char[n+1];				// buffer allocation
+	buffer[n] = '\0';					// end of the string
 	WideCharToMultiByte(CP_UTF8, NULL, wide_buffer, n, buffer, n, NULL, NULL);
 										// convert string from utf-16 buffer, to the cp 1251 
 										// and write it to the second buffer
